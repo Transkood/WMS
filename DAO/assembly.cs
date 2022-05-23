7e47cb5ccc59b5d2de73eb37c0f6d2e1e0d7e3f2
Andrey <93141508+svpra@users.noreply.github.com>
2022-05-23 18:13:35 +0300

    Create assembly.cs

---
+++ DAO/assembly.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+
+[assembly : AssemblyTitle("Warehouse Management System")]
+[assembly : AssemblyDescription("Just my system :)")]
+[assembly : AssemblyConfiguration("svpra")]
+[assembly : AssemblyCompany("Transkood")]
+[assembly : AssemblyProduct("wms-by-transkood")]
+[assembly : AssemblyCopyright("@svpra")]
+[assembly : AssemblyTrademark("Transkood")]
+[assembly : AssemblyCulture("")]
+
+[assembly : AssemblyVersion("1.0.0.0")]
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
+
