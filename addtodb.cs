125598c2f16506a5c1375b33eaaf5e2f125095c3
Andrey <93141508+svpra@users.noreply.github.com>
2022-05-26 17:26:30 +0300

    Update addtodb.cs

--- addtodb.cs
+++ addtodb.cs
@@ -11,18 +11,34 @@ namespace WMS_i_guess
     {
         public static void Main(string[] args) 
         {
-           Console.WriteLine("Введите элементы в следующем порядке: сначала наименование,далее вес, город отправления, город прибывания, контрактную компанию, количество. Если что-либо отсутствует,напишите тире");
+           Console.WriteLine("Введите элементы в следующем порядке: сначала наименование, далее вес, город отправления, город прибывания, контрактную компанию, количество. Если что-либо отсутствует,напишите тире");
            
            // разделение по запятым 
            string phrase = Console.ReadLine();
            string[] words = phrase.Split(', ');
 
+
            foreach (var word in words)
            {
                // пока что вывод в консоль, завтра подредактирую, а то сейчас лень
                Console.WriteLine($"<{word}>");
-               // место для реализации добавления в бд по индексу слова в {words}
+               // разделяем по индексу
            }
+        }
+    }
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Cargo> cargos { get; set; } = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                    add.Cargo(name, weight, cityout, cityin, company, howmany)
+            );
         }
     }
 }
