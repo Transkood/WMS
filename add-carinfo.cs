6f0ff0bb724b6f72c87c231cb933d16acf358c0c
Andrey <93141508+svpra@users.noreply.github.com>
2022-05-28 22:34:08 +0300

    Create add-carinfo.cs

---
+++ add-carinfo.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.Entity.Framework;
+
+namespace WMS_i_guess {
+    
+    class WMS { 
+        static void Main(string[] args) {
+            
+            Console.ReadLine() = carindex;
+            
