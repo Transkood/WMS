04075f55b22cd9d1723c40fc10bd9879b5525d82
Andrey <93141508+svpra@users.noreply.github.com>
2022-06-03 18:39:50 +0300

    Create roles.cs

---
+++ roles.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.Entity.Framework;
+
+namespace WMS {
+    
+    class Program {
+        static void Main(string[] args) {
+        
+            public void admin() {
+                
+            }
+        }
+    }
+}
