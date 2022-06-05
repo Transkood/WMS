e7251bc82bd813f53342bb9fa13ffbf92e391431
Andrey <93141508+svpra@users.noreply.github.com>
2022-06-05 16:15:39 +0300

    Update roles.cs

--- roles.cs
+++ roles.cs
@@ -1,13 +1,26 @@
 using System;
 using Microsoft.Entity.Framework;
 
-namespace WMS {
-    
-    class Program {
-        static void Main(string[] args) {
-        
-            public void admin() {
-                
+namespace WMS 
+{ 
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            public class ApplicationContext : DbContext
+            {
+                public DbSet<Users> cargos { get; set; } = null!;
+                public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+                {
+                    Database.EnsureCreated();   // создаем базу данных при первом обращении
+                }
+                protected override void OnModelCreating(ModelBuilder modelBuilder)
+                {
+                    modelBuilder.Entity<User>().HasData(
+                            add.User(name, role, password, login)
+                    );
+ 
+                }    
             }
         }
     }
