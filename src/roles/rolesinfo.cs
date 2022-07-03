d7f4c85002a6b4ae819171fad8eec28bc8421794
Andrey <93141508+svpra@users.noreply.github.com>
2022-07-03 21:10:19 +0300

    Create rolesinfo.cs

---
+++ src/roles/rolesinfo.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Roles {
+    static void Main(string[] args) {
+        private void InfoRoles {
+            var Admin {
+                CanAddTransportInfo = true;
+                CanAddItemInfo = true;
+                CanSeeLog = true;
+                CanSeeDB = true;
+                CanSeeUsers = true;
+                CanSeeItemLog = true;
+                
+            var Gruzchik {
+                CanAddTransportInfo = true;
+                CanAddItemInfo = true;
+                CanSeeLog = true;
+                CanSeeDB = true;
+                CanSeeUsers = true;
+                CanSeeItemLog = true;
+            }
+            var Priem {
+                CanAddTransportInfo = true;
+                CanAddItemInfo = true;
+                CanSeeLog = true;
+                CanSeeDB = true;
+                CanSeeUsers = true;
+                CanSeeItemLog = true;
+            }
+        }
+    }
+}
